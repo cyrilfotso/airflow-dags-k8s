da325dc9c8c793e6177db0efe7967ceff66e8950
Cyril FOTSO WOGUIA 1800818 <cyril.fotsowoguia@cvshealth.com>
2021-08-24 00:24:11 -0400

    adding simple run function

--- src/opp_id.py
+++ src/opp_id.py
@@ -19,17 +19,18 @@ def set_pattern(x):
 
 def data_processing():
     sleep_times = [1, 4, 5, 10]
+    print('sleep_times', sleep_times)
     df = pd.read_csv(os.path.join(PARENT_PATH, 'data', 'tweets.csv'))
-    df.head(10)
+    print(df.head(10))
     time.sleep(random.choice(sleep_times))
     df.dropna(inplace=True)
-    df.dtypes
+    print(df.dtypes)
     df['Tweet Location'] = df['Tweet Location'].astype('string')
     df['Tweet Content'] = df['Tweet Content'].str.lower()
     df['Tweet Location'].unique()
     df['Tweet Location'] = df['Tweet Location'].apply(lambda x: set_pattern(x))
     time.sleep(random.choice(sleep_times))
-    df.head(20)
+    print(df.head(20))
     return df
 
 
